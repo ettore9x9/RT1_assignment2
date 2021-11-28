83af794f93b8452c4e50c4c02e2179830854410b
ettore9x9 <ettoresani0@gmail.com>
2021-11-28 17:04:39 +0000

    keyboard_pilot_node and comments

---
+++ src/keyboard_pilot.cpp
@@ -0,0 +1,116 @@
+/* Run with: $ roslaunch rt1_assignment2 ass2.launch */
+
+/* LIBRARIES */
+#include "ros/ros.h"
+#include "geometry_msgs/Twist.h"
+#include "sensor_msgs/LaserScan.h"
+#include "rt1_assignment2/Command.h"
+#include "std_srvs/Empty.h"
+#include "stdio.h"
+#include "unistd.h"
+#include "termios.h"
+
+/* Defines a raw string literal to print the title of the console. */
+const char * title = R"(
+
+   ##################################################################
+
+    _              _                         _         _ _       _   
+   | |            | |                       | |       (_) |     | |  
+   | | _____ _   _| |__   ___   __ _ _ __ __| |  _ __  _| | ___ | |_ 
+   | |/ / _ \ | | | '_ \ / _ \ / _` | '__/ _` | | '_ \| | |/ _ \| __|
+   |   <  __/ |_| | |_) | (_) | (_| | | | (_| | | |_) | | | (_) | |_ 
+   |_|\_\___|\__, |_.__/ \___/ \__,_|_|  \__,_| | .__/|_|_|\___/ \__|
+              __/ |                             | |                  
+             |___/                              |_|                 
+
+
+   ##################################################################
+
+             
+   s : increase max speed of the robot.
+   a : decrease max speed of the robot.
+   r : put the robot in the initial position.
+   q : quit the keyboard pilot.
+
+
+)";
+
+char getch( void ) {
+   /* Function to acquire a character from keyboard input, setting the attributes of the terminal to avoid pressing 
+      enter every time. 
+      It returns the character typed from the user. */
+
+   struct termios oldt, newt; // Structs for termilal attributes.
+
+   tcgetattr( STDIN_FILENO, &oldt ); // Gets the old attributes.
+   newt = oldt;
+
+   /* Changes terminal attributes. */
+   newt.c_lflag &= ~(ICANON | ECHO);
+   newt.c_lflag |= IGNBRK;
+   newt.c_lflag &= ~(INLCR | ICRNL | IXON | IXOFF);
+   newt.c_lflag &= ~(ICANON | ECHO | ECHOK | ECHOE | ECHONL | ISIG | IEXTEN);
+   newt.c_cc[VMIN] = 1;
+   newt.c_cc[VTIME] = 0;
+
+   tcsetattr( STDIN_FILENO, TCSANOW, &newt ); // Sets new attributes.
+
+   char ch = getchar(); // Gets the input character.
+
+   tcsetattr( STDIN_FILENO, TCSANOW, &oldt ); // Sets old attributes
+
+   return ch;
+
+}
+
+/* MAIN */
+int main ( int argc, char ** argv ) {
+
+   /* Init the ros node. */
+   ros::init(argc, argv, "pilot");
+   ros::NodeHandle nh;
+
+   /* Creates a client for command service to control the speed of the robot, to reset the position, 
+      and to exit from the simulation. */
+   ros::ServiceClient client_com = nh.serviceClient<rt1_assignment2::Command>("/command");
+
+   /* Creates a client for reset_position to reset the position of the robot in the world. */
+   ros::ServiceClient client_res = nh.serviceClient<std_srvs::Empty>("/reset_positions");
+
+   rt1_assignment2::Command srv_com;
+   std_srvs::Empty srv_res;
+
+   printf( "%s", title );
+   printf( "\rSpeed: 0.00 | Awaiting command...       \r" );
+
+   char key = ' '; // Initialize the variable key.
+
+   /* While the user does not press the q and the roscore is running, executes this loop. */
+   while ( key != 'q' && ros::ok() ) {
+
+      key = getch(); // Gets the user command.
+
+      if ( key == 'a' || key == 's' || key == 'r' || key == 'q' ) { // If the command is valid.
+
+         srv_com.request.command = key;
+
+         client_com.waitForExistence();
+
+         if ( client_com.call(srv_com) ) { // Calls the command service.
+
+            if ( key == 'r' ) { // If the reset key is pressed.
+               client_res.waitForExistence();
+               client_res.call(srv_res); // Calls the reset_position service.
+            }
+            printf( "\rSpeed: %.2f | Last command: %c            \r", srv_com.response.max_speed, key );
+         }
+      } else { // The command is not valid.
+         printf( "\rCommand %c NOT valid                                                  \r", key );
+      }
+   }
+
+   printf("\n \n");
+
+   return 0;
+}
--- src/racer.cpp
+++ src/racer.cpp
@@ -1,7 +1,6 @@
-/* rosrun stage_ros stageros $(rospack find RT1_assignment2)/world/my_world.world 
-   rosrun RT1_assignment2 racer_node 
-   rosrun RT1_assignment2 pilot_teleop_key_node */
+/* Run with: $ roslaunch rt1_assignment2 ass2.launch */
 
+/* LIBRARIES */
 #include "ros/ros.h"
 #include "algorithm"
 #include "cmath"
@@ -9,56 +8,60 @@
 #include "sensor_msgs/LaserScan.h"
 #include "rt1_assignment2/Command.h"
 
+/* FUNCTION HEADERS */
 void functionCallback ( const sensor_msgs::LaserScan::ConstPtr& msg );
 void scanSectors( float * ranges, float * sectors );
-int logic( float * sectors, geometry_msgs::Twist my_vel );
-void integral_logic( float * ranges, geometry_msgs::Twist my_vel );
+int logic( float * sectors );
+void integral_logic( float * ranges );
 double integral( float * values, int start, int end );
-void drive( float straight, float turn, geometry_msgs::Twist my_vel );
+void drive( float straight, float turn );
 bool server_response( rt1_assignment2::Command::Request &req, rt1_assignment2::Command::Response &res );
 
-float d_br; // float: Alert distance for avoiding obstacles, distance break.
-float speed;
+/* GLOBAL VARIABLES */
+float d_br;                                   // Alert distance for avoiding obstacles, distance break.
+float speed;								  // Speed of the robot.
+int nsect = 9; 								  // Number of sectors.
+int front = std::floor( nsect / 2 );          // Index of the frontal sector.
+int sector_nelem = std::floor( 720/nsect );   // Number of laser surveys per sector.
+ros::Publisher pub;                           // Publisher on cmd_vel.
 
-int nsect = 7; // int: Number of sectors in which the space around the robot is divided.
-int front = std::floor( nsect / 2 );
-int sector_nelem = std::floor( 721/nsect );
-
-ros::Publisher pub; // Global, so I can use it also in the Callback.
-
+/* FUNCTIONS */
 void functionCallback ( const sensor_msgs::LaserScan::ConstPtr& msg ) {
+	/*
+	Function callback for the base_ scan subscriber, it is executed each time something is published 
+	in this specific topic.
+	*/
 
-	float sectors[nsect];
-	float ranges[721];
-	fflush(stdout);
-	std::fill_n(sectors, nsect, 10);
+	/* Preallocates variables. */
+	float sectors[nsect];             // Closest distance from obstacle in each sector.
+	float ranges[720];                // Copy of the laser_scan ranges.
 
-	geometry_msgs::Twist my_vel;
+	std::fill_n(sectors, nsect, 10);  // Initializes the values in the sectors float array to 10.
 
-	for (int i = 0; i < 721; i++) {
-		ranges[i] = msg->ranges[i];
+	/* Copies the ranges values in the base_scan message into an array. */
+	for (int i = 0; i < 720; i++) {
+		ranges[i] = msg -> ranges[i];
 	}
 
-	scanSectors(ranges, sectors);
+	/* Calls the function scanSector to fill the sectors array. */
+	scanSectors( ranges, sectors );
 
-	if ( !logic(sectors, my_vel) ) {
+	/* Call the function logic to decide what to do. If it can not take a decision, then call the function
+	   integral_logic. */
+	if ( !logic( sectors ) ) {
 
-		integral_logic(ranges, my_vel);
+		integral_logic( ranges );
 	}
 }
 
-void scanSectors(float * ranges, float * sectors) {
+void scanSectors( float * ranges, float * sectors ) {
     /*
-    Function to search for the closest gold token in each sector.
-
-    Return:
-    dist_scan: float array, every element j is the smallest distance from a golden token
-               detected in j-th sector.
+    Function to search for the closest obstacle in each sector.
     */
-    for ( int i = 1; i <= nsect; i++ ) {
-    	for ( int j = 0; j < sector_nelem; j++) {
+    for ( int i = 1; i <= nsect; i++ ) {           // For all sectors.
+    	for ( int j = 0; j < sector_nelem; j++) {  // For all elements in each sector.
 
-    		// If it finds a closest token, than update the dist_scan array.
+    		/* If it finds a closest obstacle, than update the sectors array. */
     		if ( ranges[i*sector_nelem+j] < sectors[i]) {
     		    sectors[i] = ranges[i*sector_nelem+j];
     		}
@@ -68,73 +71,78 @@ void scanSectors(float * ranges, float * sectors) {
     }
 }
 
-int logic( float * sectors, geometry_msgs::Twist my_vel ) {
+int logic( float * sectors ) {
+	/*
+    Function that rapresents the robot's behaviour. It makes choices based on the values into the sectors array.
 
-	double right_area, left_area;
+    Returns: 1 choice made
+    		 0 choice not made
+    */
 
-	if ( sectors[front] > d_br ) {
+	if ( sectors[front] > d_br ) { // The frontal sector is obstacle-free.
 
-		ROS_INFO("front distance : %f, free.", sectors[front]);
+		/* Searchs in the front-right and in the front-left sectors if there are obstacles, to line up with
+		   the track. */
+		if ( (sectors[front+1] <= 0.8 * d_br) && (sectors[front-1] >= d_br) ) {
+			ROS_INFO("dist: %.2f, speed: %.2f, free road, turn right", sectors[front], speed);
+            drive( speed, -1.5 );
 
-		if ( sectors[front+1] <= 0.6 * d_br ) {
-            drive( speed/2, -2.5/speed, my_vel );
-            return 1;
-
-		} else if ( sectors[front-1] <= 0.6 * d_br ) {
-            drive( speed/2, 2.5/speed, my_vel );
-            return 1;
-		}
-
-		if ( sectors[front] > 2 * d_br ) {
-		    drive( speed, 0.0, my_vel );
-		    return 1;
+		} else if ( (sectors[front-1] <= 0.8 * d_br) && (sectors[front+1] >= d_br) ) {
+			ROS_INFO("dist: %.2f, speed: %.2f, free road, turn left", sectors[front], speed);
+            drive( speed, 1.5 );
 
 		} else {
-		    float slow_down = ( (3 * sectors[front]) / (4 * d_br) ) - 1/2;
-		    drive( speed * slow_down, 0.0, my_vel );
-		    return 1;
+			ROS_INFO("dist: %.2f, speed: %.2f, free road", sectors[front], speed);
+		    drive( speed, 0 );
 		}
 
-	} else {
+		return 1;
 
+	} else { // There is an obstacle in the frontal sector.
+
+		/* Searchs if there is an obstacle-free sector. */
 		for ( int j = 1; j <= ( front - 1 ); j++ ) { // Looks in all sectors without the frontal one.
 
-		    if ( (sectors[front+j] >= 1.5 * d_br ) && ( sectors[front+j] >= sectors[front-j] ) ){ // First looks left.
-		        drive( 0.2, 1.0, my_vel );
-		        ROS_INFO("front distance : %f, obstacle, left free.", sectors[front]);
+		    if ( (sectors[front+j] >= d_br ) && ( sectors[front+j] >= sectors[front-j] ) ){ // First looks left.
+		    	ROS_INFO("dist: %.2f, speed: %.2f, obstacle , turn left", sectors[front], 0.2);
+		        drive( 0.2, 4 );
 		        return 1;
 
-		    } else if ( ( sectors[front-j] >= 1.5 * d_br ) && ( sectors[front-j] >= sectors[front+j] ) ) { // Then looks right.
-		        drive( 0.2, -1.0, my_vel );
-		        ROS_INFO("front distance : %f, obstacle, right free.", sectors[front]);
+		    } else if ( ( sectors[front-j] >= d_br ) && ( sectors[front-j] >= sectors[front+j] ) ) { // Then looks right.
+		        drive( 0.2, -4 );
+		        ROS_INFO("dist: %.2f, speed: %.2f, obstacle , turn right", sectors[front], 0.2);
 		        return 1;
 		    }
 		}
-		return 0;
 	}
-	return -1;
+	/* If there is not one obstacle-free sector, then it can not make any choice.*/
+	return 0;
 }
 
-void integral_logic( float * ranges, geometry_msgs::Twist my_vel ) {
+void integral_logic( float * ranges ) {
+	/*
+    Function to decide where to go when there are obstales all around the robot.
+    The choice made is based on the free area swept by the laser scan.
+    */
 
-	double right_area = integral( ranges, 0, 360 );
-	double left_area = integral( ranges, 360, 721 );
-
-		ROS_INFO("OBSTACLE, left-side area : %.2f", left_area);
-		ROS_INFO("OBSTACLE, rigt-side area : %.2f", right_area);
+	double right_area = integral( ranges, 0, 360 );   // Right-side free area.
+	double left_area = integral( ranges, 360, 720 );  // Left-side free area.
 
 		if ( right_area > left_area ) {
-			drive( 0.2, -8.0, my_vel );
-
-		} else if ( left_area > right_area ){
-			drive( 0.2, 8.0, my_vel );
+			ROS_INFO("area: %.2f, speed: %.2f, OBSTACLE , turn right", right_area, 0.2);
+			drive( 0.2, -8 );
 
 		} else {
-			drive( 0.0, 1.0, my_vel );
+			ROS_INFO("area: %.2f, speed: %.2f, OBSTACLE , turn left", left_area, 0.2);
+			drive( 0.2, 8 );
+
 		}
 }
 
 double integral( float * values, int start, int end ) {
+	/*
+    Function to perform a discrete integral with the trapezium method.
+    */
 
 	double result = 0;
 	for ( int i = start; i < end; i++ ) {
@@ -143,47 +151,70 @@ double integral( float * values, int start, int end ) {
 	return result;
 }
 
-void drive( float straight, float turn, geometry_msgs::Twist my_vel ) {
+void drive( float straight, float turn ) {
+	/*
+    Function to drive the robot, filling the geometry message and publishing it on the topic cmd_vel.
+    */
 
+	geometry_msgs::Twist my_vel;
 	my_vel.linear.x = straight;
 	my_vel.angular.z = turn;
 	pub.publish(my_vel);
 }
 
 bool server_response( rt1_assignment2::Command::Request &req, rt1_assignment2::Command::Response &res ) {
-
-	ROS_INFO("Begin server response");
+	/*
+    Function callback to the command service, it sets the speed of the motor depending on the message 
+    received and replies with the updated velocity.
+    */
 
 	if ( req.command == 'a' && speed > 0 ) {
 		ROS_INFO("Decrease speed");
-		speed = speed - 0.5;
+		speed = speed - 0.1;
 	}
 	if ( req.command == 's' ) {
 		ROS_INFO("Increase speed");
-		speed = speed + 0.5;
+		speed = speed + 0.1;
+	}
+	if ( req.command == 'r' ) {
+		ROS_INFO("Resetted");
+		speed = 0.0;
+	}
+	if ( req.command == 'q' ) {
+		ROS_INFO("Exit");
+		sleep(2);
+		exit(0);
 	}
 
-	d_br = 1.5 + speed / 50;
+	/* The distance break depends on the velocity, when the robot has an higer speed it increases the d_br variable. */
+	d_br = 1.2 + speed/18;
 
 	res.max_speed = speed;
 
 	return true;
 }
 
+/* MAIN */
 int main ( int argc, char ** argv ) {
-  
+  	
+  	/* Init the ros node. */
 	ros::init(argc, argv, "racer");
-
 	ros::NodeHandle nh;
 
+	/* Initialises the values of the global variables speed and d_br. */
 	speed = 0;
-	d_br = 1.5;
+	d_br = 1.2;
 
-	ros::Subscriber sub = nh.subscribe("base_scan", 1, functionCallback);
+	/* Subscribes to the topic base_scan to receive informations about the distance from obstacles from the 
+	   laser scanner on the robot. */
+	ros::Subscriber sub = nh.subscribe("/base_scan", 1, functionCallback);
 
+	/* Creates a service for the keyboard_pilot_node to control the speed of the robot, to reset the position 
+	   and to exit from the simulation. */
 	ros::ServiceServer service = nh.advertiseService("/command", server_response);
 
-	pub = nh.advertise<geometry_msgs::Twist>("cmd_vel", 1);
+	/* Creates a publisher to the cmd_vel topic, to guide the robot in real-time controlling its velocity. */
+	pub = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
 	
 	ros::spin();
 
